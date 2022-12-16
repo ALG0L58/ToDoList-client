badac31350c05c0d9b1205d38cd4d8f7fd9ad0bb
_BLacK_JacK_ <iiggy2661@gmail.com>
2022-12-16 15:31:33 +0200

    Update root component( -App in which the rendering tasks are implemented )

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import { useState } from 'react'
-import { useAppSelector } from './hooks/redux';
-
+import { useEffect, useMemo, useState } from 'react'
+import TodoFilter from './components/TodoFilter';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import MyInput from './components/UI/MyInput/MyInput';
+import { useAppDispatch, useAppSelector } from './hooks/redux';
+import { useTodos } from './hooks/useTodos';
+import { fetchTodos } from './store/reducers/ActionCreators';
+import { filterTodoSlice } from './store/reducers/FilterTodoSlice';
 
 function App() {
+  const dispatch = useAppDispatch()
+  const {todos, isLoading, error} = useAppSelector(state => state.todoReducer)
+  const {filteredTodos, select} = useAppSelector(state => state.filterTodoReducer)
+  const [searchQuery, setsearchQuery] = useState<string>('')
+  const selectedTodos = useTodos(filteredTodos, searchQuery)
+
+  useEffect(() => {
+      dispatch(fetchTodos())
+  },[])
+
+  useEffect(() => {
+    if(select == "ALL") {
+      dispatch(filterTodoSlice.actions.allTodos(todos))
+    }
+    if(select == "ACTIVE") {
+      dispatch(filterTodoSlice.actions.activeTodos(todos))
+    }
+    if(select == "DONE") {
+      dispatch(filterTodoSlice.actions.doneTodos(todos))
+    }
+  },[todos])
 
   return (
     <div>
+      {isLoading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      <MyInput 
+        value={searchQuery}
+        onChange={e => setsearchQuery(e.target.value)}
+        placeholder='type to search'
+      />
+      <TodoForm />
+      <TodoList todos={selectedTodos} />
+      <TodoFilter />
     </div>
   );
 }
