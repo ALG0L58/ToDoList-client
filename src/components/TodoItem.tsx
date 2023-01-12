45b58795d150bf8c909c54c3ad50ed3c5189259a
_BLacK_JacK_ <iiggy2661@gmail.com>
2023-01-12 11:42:38 +0200

    Add styles and the ability to change them, when interacting with buttons

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,50 +4,80 @@ import { changeTodo, removeTodo } from "../store/reducers/ActionCreators";
 import { TodoItemProps } from "../types/components/TodoItem";
 import MyButton from "./UI/MyButton/MyButton";
 import MyInput from "./UI/MyInput/MyInput";
+import '../styles/App/components/TodoItem/TodoItem.css'
 
 const TodoItem:FC<TodoItemProps> = ({todo}) => {
-    const {title, completed, _id} = todo
+    const {title, completed, important, _id} = todo
+    const [visibleMenu, setVisibleMenu] = useState<boolean>(false)
     const [visibleInput, setVisibleInput] = useState<boolean>(false)
     const [changeTitle, setChangeTile] = useState<string>("")
     const dispatch = useAppDispatch()
+
+    let className: string = "task__title"
+
+    if (completed) {
+        className += " task__title_completed"
+    }
+    if (important) {
+        className += " task__title_important"
+    }
 
     const changeCurrentTitle = () => {
         dispatch(changeTodo(_id, title, "title", changeTitle, false))
         setChangeTile("")
         setVisibleInput(false)
+        setVisibleMenu(!visibleMenu)
     }
 
     const currentTitle = <span
         onClick={() => dispatch(changeTodo(_id, title, "completed", "", !completed))}
+        className={className}
     >{title}</span>
 
     const changeInput = <MyInput 
         value={changeTitle}
         onChange={e => setChangeTile(e.target.value)}
         onBlur={changeCurrentTitle}
-        placeholder="change todo name"
+        placeholder="Change todo name"
+        className="my-input-change-title"
     />
 
     return (
-        <div>
+        <div className="to-do-list__task task">
             {visibleInput? 
                 changeInput
             :
                 <>-{currentTitle}</>
             }
-            <MyButton>+</MyButton>
-            <div>
+            <MyButton 
+                className={!visibleMenu? 
+                    'task__open-menu-button_visible' : 
+                    'task__open-menu-button_invisible'}
+                onHandler={() => setVisibleMenu(!visibleMenu)}
+            />
+            <div className={visibleMenu? 
+                'task__menu_visible' : 
+                'task__menu_invisible'}>
+
                 <MyButton
                     onHandler={() => setVisibleInput(!visibleInput)}
-                >change
-                </MyButton>
+                    className='task__change-button'
+                />
 
-                <MyButton>important</MyButton>
+                <MyButton 
+                    onHandler={() => 
+                        {
+                            dispatch(changeTodo(_id, title, "important", "", !important))
+                            setVisibleMenu(!visibleMenu)
+                        }
+                    }
+                    className='task__important-button'
+                />
 
                 <MyButton
                     onHandler={() => dispatch(removeTodo(title, _id))}
-                >remove
-                </MyButton>
+                    className='task__remove-button'
+                />
             </div>
         </div>
     );
