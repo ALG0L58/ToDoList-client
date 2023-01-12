5347365983c3016457c0a7d4a52b00898f09231c
_BLacK_JacK_ <iiggy2661@gmail.com>
2023-01-12 11:37:23 +0200

    Add styles and change the new task template(add an `important` field)

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -4,6 +4,7 @@ import { ITodo } from "../types/ITodo";
 import { addTodo } from '../store/reducers/ActionCreators';
 import MyButton from "./UI/MyButton/MyButton";
 import MyInput from "./UI/MyInput/MyInput";
+import '../styles/App/components/TodoForm/TodoForm.css'
 
 const TodoForm = () => {
     const [title, setTitle] = useState<string>('')
@@ -15,23 +16,26 @@ const TodoForm = () => {
         const newTodo: ITodo = {
             _id: Date.now(),
             title: title,
-            completed: false
+            completed: false,
+            important: false,
         }
         dispatch(addTodo(title, newTodo))
         setTitle('')
     }
 
     return (
-        <form>
+        <form className='to-do-list__form'>
             <MyInput 
                 value={title}
                 onChange={e => setTitle(e.target.value)}
                 placeholder='Add todo'
+                className='my-input-add-todo'
             />
             <MyButton
                 onHandler={e => addNewTodo(e)}
-            >add todo
-            </MyButton>
+                className='to-do-list__button-form'
+            />
+            
         </form>
     );
 };
