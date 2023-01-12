76c4e3008ba273df0f30226787534b18e83fb12c
_BLacK_JacK_ <iiggy2661@gmail.com>
2023-01-12 11:33:08 +0200

    Add styles and the ability to change styles when changing the filter selector

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -1,34 +1,50 @@
 import { useAppDispatch, useAppSelector } from "../hooks/redux";
 import { filterTodoSlice } from "../store/reducers/FilterTodoSlice";
 import MyButton from "./UI/MyButton/MyButton";
+import '../styles/App/components/TodoFilter/TodoFilter.css'
 
 
 const TodoFilter = () => {
     const dispatch = useAppDispatch()
     const {todos} = useAppSelector(state => state.todoReducer)
+    const {select} = useAppSelector(state => state.filterTodoReducer)
 
     return (
-        <div>
-            <MyButton
-                onHandler={() => {
-                    dispatch(filterTodoSlice.actions.getFilteredTodos(todos, "ALL", false))
-                }}
-            >All
-            </MyButton>
-
-            <MyButton
-                onHandler={() => {
-                    dispatch(filterTodoSlice.actions.getFilteredTodos(todos, "ACTIVE", false))
-                }}
-            >Active
-            </MyButton>
+        <div className="to-do-list__filter filter">
+            <div className="filter__content">
+                <MyButton
+                    onHandler={() => {
+                        dispatch(filterTodoSlice.actions.getFilteredTodos(todos, "ALL", false))
+                    }}
+                    className={
+                        select === "ALL"? 
+                        "filter__button filter__button_active" : "filter__button"
+                    }
+                >All
+                </MyButton>
+                <MyButton
+                    onHandler={() => {
+                        dispatch(filterTodoSlice.actions.getFilteredTodos(todos, "ACTIVE", false))
+                    }}
+                    className={
+                        select === "ACTIVE"? 
+                        "filter__button filter__button_active" : "filter__button"
+                    }
+                >Active
+                </MyButton>
+                
+                <MyButton
+                    onHandler={() => {
+                        dispatch(filterTodoSlice.actions.getFilteredTodos(todos, "DONE", true))
+                    }}
+                    className={
+                        select === "DONE"? 
+                        "filter__button filter__button_active" : "filter__button"
+                    }
+                >Done
+                </MyButton>
+            </div>
             
-            <MyButton
-                onHandler={() => {
-                    dispatch(filterTodoSlice.actions.getFilteredTodos(todos, "DONE", true))
-                }}
-            >Done
-            </MyButton>
         </div>
     );
 };
