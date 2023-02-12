b11a9bb3b89e4861e6bee6a4c062fc5239fc0596
_BLacK_JacK_ <iiggy2661@gmail.com>
2023-02-12 03:02:49 +0200

    Change App ( -add automatic authorization when re-entering the site -add loading tasks when the user has logged in or changed the number of tasks -add routing without reloading the site )

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,73 @@
 import { useEffect, useState } from 'react'
 import TodoFilter from './components/TodoFilter';
-import TodoForm from './components/TodoForm';
+import Registration from './components/Registration';
 import TodoList from './components/TodoList';
-import MyInput from './components/UI/MyInput/MyInput';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { useRenderingTodos } from './hooks/useRenderingTodos';
 import { useTodos } from './hooks/useTodos';
-import { fetchTodos } from './store/reducers/ActionCreators';
-import './styles/App/components/UI/MyInput//MyInput.css'
+import { auth, fetchTodos } from './store/reducers/ActionCreators';
+import './styles/App/components/UI/MyInput/MyInput.css'
 import './styles/App/App.css'
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from './components/Login';
+import Notfoundpage from './components/Notfoundpage';
+import Loading from './components/UI/Loading';
+import SearchAndAddTodo from './components/SearchAndAddTodo';
+
 
 
 function App() {
   const dispatch = useAppDispatch()
-  const {todos, isLoading, error} = useAppSelector(state => state.todoReducer)
+  const {todos, isLoading, error, version} = useAppSelector(state => state.todoReducer)
   const {filteredTodos, select} = useAppSelector(state => state.filterTodoReducer)
+  const {isAuth, currentUserId} = useAppSelector(state => state.userReducer)
   const [searchQuery, setsearchQuery] = useState<string>('')
   const selectedTodos = useTodos(filteredTodos, searchQuery)
 
+  useEffect (() => {
+    dispatch(auth())
+  })
+  
   useEffect(() => {
-    dispatch(fetchTodos())
-  },[])
-
+    if (isAuth) {
+      dispatch(fetchTodos(currentUserId))
+    }
+  },[isAuth, version])
+  
   useRenderingTodos(select, todos) 
 
-  return (
-    <div className='to-do-list'>
-      <div className='to-do-list__content'>
-        {isLoading && <h1>Loading...</h1>}
-        {error && <h1>{error}</h1>}
-        <div className='to-do-list__search-and-add-todo'>
-          <div className='to-do-list__search-query'>
-            <MyInput 
-              value={searchQuery}
-              onChange={e => setsearchQuery(e.target.value)}
-              placeholder='Type to search'
-              id='searchInput'
-              className='my-input-search-query'
-            />
-            <label htmlFor='searchInput' className='my-input-search-query__lable'></label>
-          </div>
-          <TodoForm />
-        </div>
-        <hr className='vertical-line' />
-        <TodoList todos={selectedTodos} />
-      </div>
-      <TodoFilter />
+  
+  const userTasks = 
+  <div className='to-do-list'>
+    <div className='to-do-list__content'>
+      <SearchAndAddTodo
+        searchQuery={searchQuery}
+        setsearchQuery={e => setsearchQuery(e.target.value)}
+      />
+      {error && <h1 style={{color: 'red'}}>{error}</h1>}
+      {isLoading && <Loading />}
+      <hr className='vertical-line' />
+      <TodoList todos={selectedTodos} />
     </div>
+    <TodoFilter />
+  </div>
+
+
+  return (
+    <BrowserRouter>
+      <>
+        {isAuth 
+        ?
+          userTasks
+        :
+          <Routes>
+            <Route path='/registration' element={<Registration />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='*' element={<Notfoundpage />}/>
+          </Routes>
+        }
+      </>
+    </BrowserRouter>
   );
 }
 
